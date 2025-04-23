cbcc6b54410334e74bb3a59a2897f0887162b706
Icarus-Code <2679873900@qq.com>
2025-04-23 20:28:45 +0800

    博客后台模块-标签管理的新增、批量删除、修改功能

--- yijun-admin/src/main/java/com/yijun/controller/TagController.java
+++ yijun-admin/src/main/java/com/yijun/controller/TagController.java
@@ -1,13 +1,15 @@
 package com.yijun.controller;
 
 import com.yijun.domain.ResponseResult;
+import com.yijun.dto.AddTagDto;
+import com.yijun.dto.EditTagDto;
 import com.yijun.dto.TagListDto;
 import com.yijun.service.TagService;
+import com.yijun.utils.BeanCopyUtils;
 import com.yijun.vo.PageVo;
+import com.yijun.domain.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/content/tag")
@@ -23,5 +25,42 @@ public class TagController {
     public ResponseResult<PageVo> list(Integer pageNum, Integer pageSize, TagListDto tagListDto) {
         //pageTagList是我们在yijun-framework工程写的方法
         return tagService.pageTagList(pageNum, pageSize, tagListDto);
+    }
+
+    @PostMapping
+    public ResponseResult add(@RequestBody AddTagDto tagDto) {
+        Tag tag = BeanCopyUtils.copyBean(tagDto, Tag.class);
+        tagService.save(tag);
+        return ResponseResult.okResult();
+    }
+
+    @DeleteMapping
+    public ResponseResult remove(@RequestParam(value = "ids") String ids) {
+        if (!ids.contains(",")) {
+            tagService.removeById(ids);
+        } else {
+            String[] idArr = ids.split(",");
+            for (String id : idArr) {
+                tagService.removeById(id);
+            }
+        }
+        return ResponseResult.okResult();
+    }
+
+    ////-------------------------------修改标签------------------------------------
+
+    @GetMapping("/{id}")
+    //①根据标签的id来查询标签
+    public ResponseResult getInfo(@PathVariable(value = "id") Long id) {
+        Tag tag = tagService.getById(id);
+        return ResponseResult.okResult(tag);
+    }
+
+    @PutMapping
+    //②根据标签的id来修改标签
+    public ResponseResult edit(@RequestBody EditTagDto tagDto) {
+        Tag tag = BeanCopyUtils.copyBean(tagDto, Tag.class);
+        tagService.updateById(tag);
+        return ResponseResult.okResult();
     }
 }
